46f36e1b92dcd371848358c1bfdbe5d2aac3a709
Dainelle Riley <dainellecriley@ufl.edu>
2015-11-06 20:50:41 -0500

    add js for form

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var form= document.getElementbyId("form");
+form.addEventListener ("click", function (){
+    var firstName =
+    document.getElementbyId("first").value
+    console.log("The firstName is:" + firstName);
+    document.getElementbyId("lastname").value
+    console.log("The lastName is:" + lastName);
+    document.getElementbyId("email").value
+    console.log("The email is:" + email);
+    document.getElementbyId("message").value
+    console.log("The message is:" + message);
+});
