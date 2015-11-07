4d56ef052a19ed3da306ce1dc400d2f4d4cbb931
Dainelle Riley <dainellecriley@ufl.edu>
2015-11-06 22:29:15 -0500

    add last listener

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+//upon double click on the image it will display Andy Warhol Rocks//
+
+function myFunction() {
+    document.getElementById("img").innerHTML = "Andy Warhol Rocks!";
+}
+
+//will change h2 color //
+var pageHeading = document.getElementById('h2');
+
+pageHeading.addEventListener('click', function () {
+    pageHeading.className = 'blue';
+});
+
+//will change phrases on mouse over //
+function over() {
+   document.write ("Basquiat Yay");
+    }
+
+    function out() {
+    document.write ("Warhol Yay");
+    }
+
+
